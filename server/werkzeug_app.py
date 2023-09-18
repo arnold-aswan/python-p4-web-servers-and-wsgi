c0945d219cfd6c867712cabef0b2cf0f1a2e6b54
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-09-18 10:58:44 +0300

    completed assignment

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application #> Run any codeinside the function
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple(         #> runs a server for a one page application without complications
+        hostname='localhost',   #> not suitable for prduction server
+        port=5555,
+        application=application
+    )
